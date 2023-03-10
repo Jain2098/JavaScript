471c34e430018197bfc49b48b71ff492668d0fa9
Himanshu <himanshu@gmail.com>
2023-03-10 17:45:32 -0500

    Lab 7 JavaScript

---
+++ main.js
@@ -0,0 +1,25 @@
+function firstone(){
+    document.getElementById("item1").style.display = "block";
+}
+
+function hide1(){
+    document.getElementById("item1").style.display = "none";
+}
+
+
+function secondone(){
+    document.getElementById("item2").style.display = "block";
+}
+
+function hide2(){
+    document.getElementById("item2").style.display = "none";
+}
+
+
+function thirdone(){
+    document.getElementById("item3").style.display = "block";
+}
+
+function hide3(){
+    document.getElementById("item3").style.display = "none";
+}
